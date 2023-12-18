585103eb29fdb53fe4bc7dc1ce67b76678c72d9e
Jonathan Chen <jonafish10@gmail.com>
2023-12-18 11:39:51 -0500

    Finish day 4

--- day_4/soln.py
+++ day_4/soln.py
@@ -2,11 +2,31 @@ import sys
 
 sys.path.append('..')
 
-def solve_A(input_lines: list):
-  pass
+def solve_A(input_lines: list[str]) -> int:
+  return sum([lotto_points(line) for line in input_lines])
 
-def solve_B(input_lines: list):
-  pass
+def lotto_points(lotto_line: str) -> int:
+  num_matches = lotto_match_count(lotto_line)
+  return 1 << (num_matches - 1) if num_matches > 0 else 0
+
+def lotto_match_count(lotto_line: str) -> int:
+  winning_nums, my_nums = parse_lotto(lotto_line)
+  return len(set(winning_nums).intersection(set(my_nums)))
+
+def parse_lotto(lotto_line: str) -> list[list[int]]:
+  # Input of the form '''Card N: N ... | N ...'''
+  lotto_line = lotto_line.split(':')[1]
+  return [[int(n) for n in part.split()] for part in lotto_line.split('|')]
+
+def solve_B(input_lines: list[str]) -> int:
+  num_lotto_cards = [1 for _ in range(len(input_lines))]
+  for line_num, lotto_line in enumerate(input_lines):
+    num_matches = lotto_match_count(lotto_line)
+    for offset in range(1, num_matches + 1):
+      num_lotto_cards[line_num + offset] += num_lotto_cards[line_num]
+  
+  return sum(num_lotto_cards)
+
 
 if __name__ == "__main__":
   input_lines = [line.rstrip() for line in sys.stdin]
