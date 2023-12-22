b9f31a5a23f27fbc5de0b0a1f599ee2d8e3c7f89
Jonathan Chen <jonafish10@gmail.com>
2023-12-22 16:23:51 -0500

    Solve day 9 using Lagrange interpolation

--- day_9/soln.py
+++ day_9/soln.py
@@ -1,12 +1,35 @@
 import sys
 
-sys.path.append('..')
+import numpy as np
 
-def solve_A(input_lines: list):
-  pass
+sys.path.append('..')
+import logger
+
+def solve_A(input_lines: list[str]) -> int:
+  running_sum = 0
+  for line in input_lines:
+    seq = [int(n) for n in line.split()]
+    xs = [i for i in range(len(seq))]
+    ys = seq
+    #logger.log(xs, ys)
+    fit = np.polynomial.Polynomial.fit(xs, ys, len(seq) - 1)
+    #logger.log(xs, ys)
+    #logger.log([fit(x) for x in xs])
+    running_sum += round(fit(len(seq)))
+  return running_sum
 
 def solve_B(input_lines: list):
-  pass
+  running_sum = 0
+  for line in input_lines:
+    seq = [int(n) for n in line.split()]
+    xs = [i for i in range(len(seq))]
+    ys = seq
+    #logger.log(xs, ys)
+    fit = np.polynomial.Polynomial.fit(xs, ys, len(seq) - 1)
+    #logger.log(xs, ys)
+    #logger.log([fit(x) for x in xs])
+    running_sum += round(fit(-1))
+  return running_sum
 
 if __name__ == "__main__":
   input_lines = [line.rstrip() for line in sys.stdin]
